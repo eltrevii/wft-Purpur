cb4bcf5d69e487e38b4f21b4aaee96fef34efa9a
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-04-11 22:34:44 +0200

    u

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-curl -k "https://api.purpurmc.org/v2/purpur/1.18.1/latest/download" -L -o purpur.jar --progress-bar
-(sleep 1h; echo stop) | java -Xmx3G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -Dlog4j.configurationFile=log4j2.xml --add-modules=jdk.incubator.vector -jar "purpur.jar" nogui &
+while true:
+	(sleep 5h; systemctl reboot) &
+	curl -k "https://api.purpurmc.org/v2/purpur/1.18.1/latest/download" -L -o purpur.jar --progress-bar
+	(sleep 1h; echo stop) | java -Xmx3G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -Dlog4j.configurationFile=log4j2.xml --add-modules=jdk.incubator.vector -jar "purpur.jar" nogui &
+	echo .
+	git add *
+	git commit -a -m "auto comit lolz"
+	git push
