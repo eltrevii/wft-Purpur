e315f29243b2b205f49fe271a3a0c7b4d829efeb
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-04-19 16:12:35 +0200

    auto comit lolz

--- start.cmd
+++ start.cmd
@@ -3,7 +3,7 @@ title Purpur
 cls
 curl -k "https://api.purpurmc.org/v2/purpur/1.18.1/latest/download" -L -o purpur.jar --progress-bar
 cls
-for %%i in (*.jar) do java -Xmx10G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -Dlog4j.configurationFile=log4j2.xml --add-modules=jdk.incubator.vector -jar "%%i" nogui
+java -Xmx10G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -Dlog4j.configurationFile=log4j2.xml --add-modules=jdk.incubator.vector -jar purpur.jar
 echo.
 
 title auto comiter xd
